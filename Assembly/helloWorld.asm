cc1ac51f6546af21e59bde59934b8d21b477ee80
Thorgaran <39826071+Thorgaran@users.noreply.github.com>
2021-01-31 02:20:37 +0100

    Use subroutines to massively shorten the program
    Met a bad stack bug on the way that was debugged thanks to the emulator
    Program has been tested on hardware and is working properly

--- Assembly/helloWorld.asm
+++ Assembly/helloWorld.asm
@@ -1,3 +1,5 @@
+SAVE_X  = $0000
+
 VIA     = $6000
 PORTB   = VIA
 PORTA   = VIA + 1
@@ -10,6 +12,79 @@ LED_E  = %00100000
 
         .org $8000
 
+lcd_init:
+        lda #%00110011  ;Set 8-bit mode twice
+        jsr lcd_cmd
+
+        lda #%00110010  ;Set 8-bit mode then 4-bit mode
+        jsr lcd_cmd
+
+        lda #%00101000  ;Function set: 4-bit mode, 2 lines, 5x8 pixels
+        jsr lcd_cmd
+
+        lda #%00000001  ;Screen clear
+        jsr lcd_cmd
+
+        lda #%00001111  ;Display switch: display ON, cursor ON, blink ON
+        jsr lcd_cmd
+
+        lda #%00000110  ;Entry set: increment, shift cursor
+        jsr lcd_cmd
+
+        rts
+
+lcd_send_1:
+        stx SAVE_X      ;Save X contents in RAM
+        tax             ;Save command into X
+        
+        lsr
+        lsr
+        lsr
+        lsr             ;Shift the 4 high-order bits into the low 4 bits
+
+        rts
+
+lcd_send_2:
+        sta PORTB       ;Send first half of the command
+
+        ora #LED_E
+        sta PORTB
+        and #~LED_E
+        sta PORTB       ;Send pulse on the Enable pin of the LCD
+
+        txa             ;Retrieve saved command
+        and #%00001111  ;Keep the 4 low-order bits
+
+        rts
+
+lcd_send_3:
+        sta PORTB       ;Send second half of the command
+
+        ora #LED_E
+        sta PORTB
+        and #~LED_E
+        sta PORTB       ;Send pulse on the Enable pin of the LCD
+
+        ldx SAVE_X      ;Retrieve X contents from RAM
+
+        rts
+
+lcd_cmd:
+        jsr lcd_send_1
+        jsr lcd_send_2
+        jsr lcd_send_3
+
+        rts
+
+lcd_data:
+        jsr lcd_send_1
+        ora #LED_RS
+        jsr lcd_send_2
+        ora #LED_RS
+        jsr lcd_send_3
+
+        rts
+        
 irq:
 nmi:
 reset:  ldx #$ff
@@ -18,330 +93,34 @@ reset:  ldx #$ff
         lda #%11101111  ;Set output B pins 
         sta DDRB        ;Initialize Port B to one input and #full output
 
-        lda #(%0 | %0011)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %0011)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %0011)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %0010)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-       
-        lda #(%0 | %0010)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %1000)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %0000)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %1111)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %0000)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(%0 | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0100)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %1000)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0101)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %1100)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %1100)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %1111)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0010)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %1100)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0010)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0000)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0101)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-        
-        lda #(LED_RS | %0111)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %1111)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0111)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0010)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %1100)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0110)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0100)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0010)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
-
-        lda #(LED_RS | %0001)
-        sta PORTB
-
-        ora #LED_E
-        sta PORTB
-
-        and #~LED_E
-        sta PORTB
+        jsr lcd_init    ;Set up LCD screen
+
+        lda #"H"
+        jsr lcd_data
+        lda #"e"
+        jsr lcd_data
+        lda #"l"
+        jsr lcd_data
+        lda #"l"
+        jsr lcd_data
+        lda #"o"
+        jsr lcd_data
+        lda #","
+        jsr lcd_data
+        lda #" "
+        jsr lcd_data
+        lda #"W"
+        jsr lcd_data
+        lda #"o"
+        jsr lcd_data
+        lda #"r"
+        jsr lcd_data
+        lda #"l"
+        jsr lcd_data
+        lda #"d"
+        jsr lcd_data
+        lda #"!"
+        jsr lcd_data
 
         stp
 
